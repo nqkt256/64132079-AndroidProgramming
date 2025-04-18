5d70be8797ca685d74fc67bbc8d50fb7a11aa97f
NqKT <nguyenquockytai@gmail.com>
2025-04-18 16:43:31 +0700

    Create Ex_Fragment_Replace

---
+++ FragmentEx_Replace/app/src/main/java/com/example/fragmentex_replace/FooterFragment.java
@@ -0,0 +1,63 @@
+package com.example.fragmentex_replace;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FooterFragment extends Fragment {
+
+
+    public FooterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_footer, container, false);
+        Button b1 = v.findViewById(R.id.btn1);
+        Button b2 = v.findViewById(R.id.btn2);
+        Button b3 = v.findViewById(R.id.btn3);
+        //
+        FragmentManager fragmentManager = getParentFragmentManager();
+        //event
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new FragmentOne())
+                        .commit();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new FragmentTwo())
+                        .commit();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new FragmentThree())
+                        .commit();
+            }
+        });
+        return v;
+    }
+}
